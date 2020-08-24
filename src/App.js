94b456e3ad8dfd036c5a3edee1912c6441abfa15
Adil Islambekov <osnapitzadi@gmail.com>
2020-08-24 05:30:04 -0400

    no idea

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css";
 import { Container, Button } from 'react-bootstrap';
-// import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList";
 
 
 
 
 class App extends Component {
   state = {
-    items: [
-      {
-        id: 11,
-        content: 'first todo',
-        done: false
-      }
-    ],
-    id: '',
-    content: "",
-    done: false
+      items: [
+        {
+          id: 11,
+          content: 'first todo',
+          done: false
+        },
+        {
+          id: 22,
+          content: 'second todo',
+          done: true
+        }
+      ],
+      id: undefined,
+      content: "",
+      done: false
   };
-    
-  inputHandler = (event) => {
 
-    console.log(this.state.content);
     
+  inputHandler = (event) => {    
     this.setState({
       content: event.target.value
     })
@@ -50,12 +53,15 @@ class App extends Component {
 
     this.setState({
       items: newItems,
-      content: ''
+      content: ""     
     });
-
-
   }
-
+  // TODO finish
+  // doneHandler = (event) => {
+  //   // const newItems = [...this.state.items]
+  //   console.log(event.target.getAttribute('id'))
+  // }
+  
 
 
   render () {
@@ -64,11 +70,9 @@ class App extends Component {
         <Header></Header>
         <input className='m-3' onChange={this.inputHandler}></input>
         <Button variant="outline-light" onClick={this.addTodo}>Add TODO</Button>
-        <ul>
-          {this.state.items.map(item => (
-            <li key={item.id}>{item.content}</li>
-          ))}
-        </ul>
+        <TodoList 
+          items={this.state.items} 
+          ></TodoList>
       </Container>
     )
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-
+import {ListGroup} from 'react-bootstrap';
+import { BsFillTrashFill } from "react-icons/bs";
 
 export default class TodoList extends Component {
     render () {
         return (
-            <div>
-                <ul>
-                {this.state.items.map(item => (
-                    <li key={item.id}>{item.content}</li>
+            <ListGroup>
+                    {this.props.items.map(item => {
+                            return (
+                                <ListGroup.Item
+                                    className='d-flex justify-content-between align-items-center'
+                                    variant='warning'
+                                    key={item.id}
+                                    // id={item.id}
+                                    // onClick={this.props.doneHandler}
+                                    >
+                                        {item.content}
+                                        <BsFillTrashFill 
+                                        className='mx-2'
+                                        // onClick={this.props.deleteHandler}
+                                        />
+                                </ListGroup.Item>
+                            )
+                        }
+                    )   
+                }
+
+                {/* <ul>
+                {this.props.items.map(item => (
+                    <li 
+                    key={item.id}
+                    >{item.content}</li>
                 ))}
-                </ul>
-            </div>
+                </ul> */}
+            </ListGroup>
         );
     };
 
