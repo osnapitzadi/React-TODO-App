8217be306a8df1f76238ea708ab38f50ddfd5ff3
Adil Islambekov <osnapitzadi@gmail.com>
2020-08-24 00:22:06 -0400

    new entire prjc

--- src/App.js
+++ src/App.js
@@ -1,90 +1,26 @@
-import React, { useState, useCallback, useEffect } from 'react';
-
-const App = () => {
-  const [newTodo, setNewTodo] = useState('');
-  const [todos, setTodos] = useState([]);
-
-  const onNewTodoChange = useCallback((event) => {
-    setNewTodo(event.target.value);
-  }, []);
-
-  const formSubmitted = useCallback((event) => {
-    event.preventDefault();
-    if (!newTodo.trim()) return;
-    setTodos([
-      {
-        id: todos.length ? todos[0].id + 1 : 1,
-        content: newTodo,
-        done: false,
-      },
-      ...todos
-    ]);
-    setNewTodo('');
-  }, [newTodo, todos]);
-
-  useEffect(() => {
-    console.log('todos', todos);
-  }, [todos]);
-
-  const addTodo = useCallback((todo, index) => (event) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1, {
-      ...todo,
-      done: !todo.done
-    });
-    setTodos(newTodos);
-  }, [todos]);
-
-  const removeTodo = useCallback((todo) => (event) => {
-    setTodos(todos.filter(otherTodo => otherTodo !== todo));
-  }, [todos]);
-
-  const markAllDone = useCallback(() => {
-    // create a copy of the array
-      // create a copy of each of the items
-        // update the done property to be true on each of the new items
-    const updatedTodos = todos.map(todo => {
-      return {
-        ...todo,
-        done: true,
-      };
-    });
-    setTodos(updatedTodos);
-  }, [todos]);
-
-  return (
-    <div className='container'>
-      <h1>Todo list Application by Adil</h1>
-      <form onSubmit={formSubmitted}>
-        <label htmlFor="newTodo">Enter a Todo:</label>
-        <input
-          id="newTodo"
-          name="newTodo"
-          value={newTodo}
-          onChange={onNewTodoChange}
-        />
-        <button className='btn btn-primary btn-sm'>Add Todo</button>
-      </form>
-      <button onClick={markAllDone}>Mark All Done</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={todo.id}>
-            <input
-              checked={todo.done}
-              type="checkbox"
-              onChange={addTodo(todo, index)}
-            />
-            <span className={todo.done ? 'done' : ''}>{todo.content}</span>
-            <button 
-              onClick={removeTodo(todo)}
-              className='btn btn-danger btn-sm'>Remove Todo</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+import React, { Component } from "react";
+import Header from "./components/Header";
 
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      done: '',
+      content: '',
+    };
+  }
 
-export default App;
+  render () {
+    return (
+      <div>
+        <Header></Header>
+      </div>
+    )
+  }
+
+}
+
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container,  Nav } from 'react-bootstrap';
+
+const Header = () => (
+
+    <Container>
+        <Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+            <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                Disabled
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    </Container>
+)
+
+export default Header;
